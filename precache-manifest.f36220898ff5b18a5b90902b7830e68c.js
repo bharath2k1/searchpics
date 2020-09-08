43525ae4484da5308e2b468d492946e44288f254
BHARATH SIMHA REDDY <bharathreddy8345@gmail.com>
2020-09-08 08:59:40 +0530

    Updates

---
+++ precache-manifest.f36220898ff5b18a5b90902b7830e68c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a29cfc655b3b0b1e2a90bb7ec45b5f37",
+    "url": "/searchpics/index.html"
+  },
+  {
+    "revision": "77d368109f43e06b98cb",
+    "url": "/searchpics/static/css/main.c00f04db.chunk.css"
+  },
+  {
+    "revision": "a807f3f0a36982b89c60",
+    "url": "/searchpics/static/js/2.6fa09cdb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/searchpics/static/js/2.6fa09cdb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "77d368109f43e06b98cb",
+    "url": "/searchpics/static/js/main.4b621c06.chunk.js"
+  },
+  {
+    "revision": "5739b0d2f50c706b8c63",
+    "url": "/searchpics/static/js/runtime-main.4b3ffaf5.js"
+  }
+]);
